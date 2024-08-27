883d7166f9d347ee565e7ee29f6048d16f008fc2
vladimir <vlj2007@yandex.ru>
2024-08-27 20:57:17 +0700

    I have finished my file Technique

--- src/main/java/ru/isands/technique/api/Technigue.java
+++
@@ -1,7 +0,0 @@
-package ru.isands.technique.api;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.isands.technique.entity.HouseholdAppliancesEntity;
-
-public interface Technigue extends JpaRepository<HouseholdAppliancesEntity, Long> {
-}
--- src/main/java/ru/isands/technique/api/Technique.java
+++ src/main/java/ru/isands/technique/api/Technique.java
@@ -3,6 +3,5 @@ package ru.isands.technique.api;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.isands.technique.entity.HouseholdAppliancesEntity;
 
-public interface technique extends JpaRepository<HouseholdAppliancesEntity, Long>{
-
+public interface Technique extends JpaRepository<HouseholdAppliancesEntity, Long> {
 }
